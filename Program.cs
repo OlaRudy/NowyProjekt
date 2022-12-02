4035b0ba5c0ba650432a96e24b61cace99661fb9
OlaRudy <119697390+OlaRudy@users.noreply.github.com>
2022-12-02 22:02:30 +0100

    1
    11

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NowyProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Ola!");
+
+            
+        }
+    }
+}
