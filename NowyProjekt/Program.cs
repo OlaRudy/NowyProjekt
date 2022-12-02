5a3cf4896eb3c7f6e0440e6b5597130c2eb05237
OlaRudy <aleksandrarudy92@gmail.com>
2022-12-02 21:59:30 +0100

    Dodaj pliki projektów.

---
+++ NowyProjekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NowyProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
